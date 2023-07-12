b64136f0e1db8296c093d65a932f07358beb12f2
ksnedeker1 <snedekek@oregonstate.edu>
2023-07-11 23:01:15 -0400

    Add htmlstring.py

---
+++ src/gui/htmlstrings.py
@@ -0,0 +1,12 @@
+td_welcome_message = """
+    <h1>Welcome to vrnImage!</h1>
+    <p>This is a work-in-progress application which will serve to demonstrate an experimental image
+    compression technique using Voronoi diagrams. This area will show explanatory material regarding
+    the compression process, as well as tips, hints, and how-tos.</p>
+    <p>Once you've compressed an image by uploading or selecting an included image file and clicking 
+    <i>'Start'</i>, you can use the buttons to the left and right of this area with '&lt;' and '&gt;' symbols 
+    to step through the process explanation. This will use examples taken directly from the image you 
+    compressed.</p>
+    <p>If you do not wish to generate and see explanatory elements, uncheck the <i>'Generate Demonstrative 
+    Elements'</i> checkbox under <i>'Advanced Settings'</i></p>
+    """
