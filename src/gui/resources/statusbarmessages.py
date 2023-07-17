c0a87c0ab8d77baf696e5574b0a057401222feb3
ksnedeker1 <snedekek@oregonstate.edu>
2023-07-17 09:57:02 -0400

    Add invalid params message

--- src/gui/resources/statusbarmessages.py
+++ src/gui/resources/statusbarmessages.py
@@ -1,2 +1,3 @@
 sb_no_image_selected = ("Cannot start: No image selected!", 5000)
 sb_image_loaded = ("Image successfully loaded.", 5000)
+sb_invalid_parameters = ("Cannot start: One or more parameters are invalid.", 5000)
