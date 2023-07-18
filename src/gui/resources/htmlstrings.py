46d1022e4e24b6b3eab2e0aaaa27f575486f73e5
ksnedeker1 <snedekek@oregonstate.edu>
2023-07-17 22:20:26 -0400

    Edit string

--- src/gui/resources/htmlstrings.py
+++ src/gui/resources/htmlstrings.py
@@ -8,12 +8,12 @@ td_welcome = f"""
     <p>This is a work-in-progress application which will serve to demonstrate an experimental image
     compression technique using Voronoi diagrams. This area will show explanatory material regarding
     the compression process, as well as tips, hints, and how-tos.</p>
-    <p>Once you've compressed an image by uploading or selecting an included image file and clicking 
+    <p>Once you've chosen an image by uploading or selecting an included image file and clicking 
     <i>'Start'</i>, you can use the buttons to the left and right of this area with '&lt;' and '&gt;' symbols 
     to step through the process explanation. This will use examples taken directly from the image you 
     compressed.</p>
     <p>If you do not wish to generate and see explanatory elements, uncheck the <i>'Generate Demonstrative 
-    Elements'</i> checkbox under <i>'Advanced Settings'</i></p>
+    Elements'</i> checkbox under <i>'Advanced Settings'</i>.</p>
     """
 
 td_image_loaded = """
