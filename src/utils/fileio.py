f9d99da316b7c0bcb2c6dcd4a77fcb4f675523de
ksnedeker1 <snedekek@oregonstate.edu>
2023-07-20 17:07:56 -0400

    Update import_image

--- src/utils/fileio.py
+++ src/utils/fileio.py
@@ -17,11 +17,15 @@ def import_image(path: str):
     Imports the image file at the specified path. Supports most common file types.
     :param path: relative or absolute path to the file
     :param split: whether output splits luminance and color channels or not
-    :return: numpy array containing CIELAB values representing the imported image
+    :return: numpy array containing RGB values representing the imported image
     """
     # Ensure path is a string which is valid and exists
     if not type(path) == str or not os.path.isfile(path):
         raise ImagePathError(path)
     # Open image with PIL
     image = Image.open(path)
+    # Remove alpha channel if png
+    # TODO: handle png alpha channel
+    if path[-3:] == 'png':
+        return np.array(image)[:,:,:3]
     return np.array(image)
