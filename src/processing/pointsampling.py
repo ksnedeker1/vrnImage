434bd40459b577b67078863516bae3883dfa8ac2
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-10 07:29:07 -0400

    Add param for sampling linearity

--- src/processing/pointsampling.py
+++ src/processing/pointsampling.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def sample_coordinates(heatmap, num_points):
+def sample_coordinates(heatmap, num_points, linearity_power=1):
     """
     Sample coordinates from a heatmap.
     :param heatmap: 2D array where the 'importance' of a coord is expressed between 0 and 1.
@@ -9,7 +9,7 @@ def sample_coordinates(heatmap, num_points):
     :returns: List of (y, x) coordinates sampled from the heatmap.
     """
     # Flatten and normalize to make a proper probability distribution
-    flat = heatmap.flatten().astype(np.float64)
+    flat = heatmap.flatten().astype(np.float64) ** linearity_power
     flat /= flat.sum()
     # Sampling and conversion back to 2D
     sampled_flat = np.random.choice(len(flat), size=num_points, replace=False, p=flat)
