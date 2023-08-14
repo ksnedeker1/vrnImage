7eaf9818ec00e7faffa619e1159e493b2d1f0ab5
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-14 19:54:08 -0400

    Add reconstruct_image()

--- src/processing/voronoiaveraging.py
+++ src/processing/voronoiaveraging.py
@@ -41,3 +41,27 @@ def voronoi_average_color_by_cell(img, vor):
     averages = [(color_sum / count).astype(np.uint8) if count != 0
                 else np.zeros(3) for color_sum, count in zip(color_sums, counts)]
     return averages
+
+
+def reconstruct_image(vor, averages, img_shape):
+    """
+    Reconstruct vrn image using average colors and coords of Voronoi cells.
+    :param vor: scipy.spatial.Voronoi(sampled coords)
+    :param averages: List of average colors for coordinate, associated by order with vor.
+    :param img_shape: Shape of the original image (height, width, channels).
+    :returns: Reconstructed image as np.array.
+    """
+    reconstructed = np.zeros(img_shape, dtype=np.uint8)
+    rows, cols = img_shape[:2]
+    # Iterate through valid regions
+    for idx, region in enumerate(vor.regions):
+        if not -1 in region and len(region) > 1:
+            # Define the polygon and clamp vertices to image bounds
+            polygon = np.array([vor.vertices[i] for i in region], dtype=np.int32)
+            polygon[:, 0] = np.clip(polygon[:, 0], 0, cols - 1)
+            polygon[:, 1] = np.clip(polygon[:, 1], 0, rows - 1)
+            # Extract average RGB color
+            avg_color_rgb = averages[idx].astype(np.uint8)
+            # Fill pixels in the current polygon (region) with RGB average extracted from averages[idx]
+            cv2.fillPoly(reconstructed, [polygon.reshape((-1, 1, 2))], color=(int(avg_color_rgb[0]), int(avg_color_rgb[1]), int(avg_color_rgb[2])))
+    return reconstructed
