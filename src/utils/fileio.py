f3e12952cb46f97a136a14399bb97883e40ad9ac
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-15 00:00:15 -0400

    Amend default path

--- src/utils/fileio.py
+++ src/utils/fileio.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import numpy as np
-import skimage.color
 from PIL import Image
 
 
@@ -32,7 +31,6 @@ def import_image(path: str):
     return np.array(image)
 
 
-def array_to_image(img_array, path='./', filename='result_tmp.jpg'):
+def array_to_image(img_array, path='./images/results', filename='result_tmp.jpg'):
     """Saves an array of RGB values as an image at 'path' with 'filename' (incl. extension)."""
     cv2.imwrite(os.path.join(path, filename), cv2.cvtColor(img_array, cv2.COLOR_RGB2BGR))
-
