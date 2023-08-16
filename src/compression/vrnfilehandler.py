8829be1c74cf69ff062ffebfb59d84a435d8b139
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-16 09:23:38 -0400

    Create compression/decompression funcs for custom vrn file format

---
+++ src/compression/vrnfilehandler.py
@@ -0,0 +1,33 @@
+import struct
+
+
+def vrn_compress(img, vor, averages, filename, directory='./'):
+    """
+    Store a collection of points and color averages as a .vrn compressed file.
+    """
+    rows, cols = img.shape[:2]
+    # Header: width, height, points count
+    data = [struct.pack('<III', cols, rows, len(vor.points))]
+    # Coordinates and Colors
+    for coord, avg in zip(vor.points, averages):
+        # little-endian, 2x unsigned short (max dim.: 65535), 3x unsigned char (1 byte int, RGB in [0, 255])
+        data.append(struct.pack('<HHBBB', int(coord[0]), int(coord[1]), int(avg[0]), int(avg[1]), int(avg[2])))
+    with open(f"{directory}/{filename}.vrn", 'wb') as f:
+        f.write(b''.join(data))
+
+
+def vrn_decompress(filename, directory='./'):
+    """
+    Decompress a .vrn file into rows, cols, num_points, coords, averages.
+    """
+    with open(f"{directory}/{filename}.vrn", 'rb') as f:
+        # Read header
+        cols, rows, num_points = struct.unpack('<III', f.read(12))
+        # unpack coords and average colors
+        coords = []
+        averages = []
+        for _ in range(num_points):
+            col, row, r, g, b = struct.unpack('<HHBBB', f.read(7))
+            coords.append((col, row))
+            averages.append((r, g, b))
+        return (rows, cols, num_points), coords, averages
