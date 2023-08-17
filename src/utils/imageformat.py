d1e3f52cbeced334c6b2b40eaf684b38fb1716fb
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-17 13:42:55 -0400

    Remove 'split' optional parameter

--- src/utils/imageformat.py
+++ src/utils/imageformat.py
@@ -2,7 +2,7 @@ import numpy as np
 import skimage.color
 
 
-def rgb_to_cielab(image, split=None):
+def rgb_to_cielab(image):
     """
     Converts an RGB image array to CIELAB.
     :param image: The RGB image array to convert
@@ -10,4 +10,4 @@ def rgb_to_cielab(image, split=None):
     :return: CIELAB version of the image array, see param split
     """
     cielab_array = skimage.color.rgb2lab(np.array(image))
-    return (cielab_array[:, :, 0], cielab_array[:, :, 1:]) if split else cielab_array
+    return cielab_array
