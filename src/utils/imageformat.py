667e36c3af693a52419bdd6dd8dbd85716bdb47b
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-18 19:36:07 -0400

    Improve documentation

--- src/utils/imageformat.py
+++ src/utils/imageformat.py
@@ -5,9 +5,6 @@ import skimage.color
 def rgb_to_cielab(image):
     """
     Converts an RGB image array to CIELAB.
-    :param image: The RGB image array to convert
-    :param split: Whether the return should split the CIELAB array between luminance and color channels
-    :return: CIELAB version of the image array, see param split
     """
     cielab_array = skimage.color.rgb2lab(np.array(image))
     return cielab_array
