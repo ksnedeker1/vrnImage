ea929f4b800ac6b25d7a6662c323f8ba8ab102c0
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-18 19:10:13 -0400

    Add status message for process completion

--- src/gui/resources/statusbarmessages.py
+++ src/gui/resources/statusbarmessages.py
@@ -4,7 +4,10 @@ sb_invalid_parameters = ("Cannot start: One or more parameters are invalid.", 50
 sb_stop_process = ("Stopping process...", 5000)
 sb_process_status = "Compressing {image}  |  Samples: {samples}, Linearity: {linearity}  |  {task}"
 
-sb_task_heatmap = "(1/4) Running edge detection and building heatmap..."
-sb_task_coords = "(2/4) Sampling coordinates..."
-sb_task_voronoi = "(3/4) Constructing Voronoi diagram..."
-sb_task_reconstruct = "(4/4) Averaging cell colors and reconstructing image..."
+sb_task_heatmap = "(1/5) Running edge detection and building heatmap..."
+sb_task_coords = "(2/5) Sampling coordinates..."
+sb_task_voronoi = "(3/5) Constructing Voronoi diagram..."
+sb_task_compressed = "(4/5) Averaging cell colors and storing compressed format..."
+sb_task_reconstruct = "(5/5) Reconstructing image..."
+
+sb_process_complete = ("Done. See Metrics tabs for more info.", 5000)
