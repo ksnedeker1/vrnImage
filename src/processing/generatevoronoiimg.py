ff5c34d973d8aa8faa2acfb2b1382912a99b6945
ksnedeker1 <snedekek@oregonstate.edu>
2023-08-18 13:47:15 -0400

    Add color palettes based on size class of Voronoi cell (quartiles)

--- src/processing/generatevoronoiimg.py
+++ src/processing/generatevoronoiimg.py
@@ -6,13 +6,34 @@ def generate_voronoi_img(shape, vor):
     """
     Create an array representing an RGB image edges of Voronoi cells colored white.
     """
-    output = np.zeros(shape, dtype=np.uint8)
+    # Approximate all areas, find 25th, 50th, 75th percentile values
+    areas = [shoelace([vor.vertices[i] for i in cell]) if -1 not in cell else 0 for cell in vor.regions]
+    q25, q50, q75 = np.percentile(areas, [25, 50, 75])
     # For each bounded cell, draw the edges of the polygon defined by the vertices of that cell
-    for cell in vor.regions:
+    output = np.zeros(shape, dtype=np.uint8)
+    for index, cell in enumerate(vor.regions):
         if -1 not in cell:
             polygon = [vor.vertices[i] for i in cell]
             if polygon:
+                # Color scheme dependent on size class of cell
+                if areas[index] < q25:
+                    color = (np.random.randint(127, 255), 0, np.random.randint(127, 255))
+                elif areas[index] < q50:
+                    color = (np.random.randint(127, 255), np.random.randint(127, 255), 0)
+                elif areas[index] < q75:
+                    color = (0, np.random.randint(127, 255), np.random.randint(127, 255))
+                else:
+                    val = np.random.randint(0, 127)
+                    color = (val, val, val)
                 polygon_reshaped = np.array(polygon, np.int32)
-                color = (np.random.randint(0, 255), 255, np.random.randint(0, 255))
                 cv2.fillPoly(output, [polygon_reshaped], color)
     return output
+
+
+def shoelace(polygon):
+    """
+    Quick shoelace approximation
+    """
+    x = [p[0] for p in polygon]
+    y = [p[1] for p in polygon]
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
